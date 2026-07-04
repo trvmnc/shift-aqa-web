a1ec937325788315cb4d1760a844a4eaf4720116
trvmnc <viktor100112@gmail.com>
2026-07-04 21:19:32 +0700

    вроде всё, где не понятно написал

--- tests/dz2.spec.ts
+++ tests/dz2.spec.ts
@@ -2,13 +2,46 @@ import {test, expect} from '@playwright/test';
 import pages from '../data/pages.json';
 
 pages.forEach((testPage: {name: string, url: string}) => {
-    test(`Соответствие pages.json сайту, ${testPage.name}`, async ({page}) => {
+    test(`Страница ${testPage.name}`, async ({page}) => {
 
+        const width = await page.evaluate(() => window.innerWidth)
+
+        await page.goto(testPage.url);
+
+        if (testPage.url == '') {
+            await expect.soft(page.getByTestId('cookie-consent-banner')).toHaveScreenshot('Плашка кук.png')
+            await page.getByTestId('cookie-accept-button').click();
+
+            if (width <= 768) {
+                await page.getByTestId('header-burger-menu-button').click()
+                await expect.soft(page.getByTestId('header-mobile-menu')).toHaveScreenshot('Бургер меню.png')
+                await page.getByTestId('header-burger-menu-button').click()
+
+            }
+        } else {
+            await page.getByTestId('cookie-accept-button').click();
+        }
+
+        await expect.soft(page).toHaveScreenshot({
+            fullPage: true,
+            mask: [
+                page.locator('[data-testid^="catalog-product-price-prod-"]'),
+                page.getByTestId('feedback-captcha-image')
+            ]
+        });
     })
-})
+});  // не понял суть задания, нужно проверить соответствие скринов сайту? если так,
+     // то не понял чем это отличается от того, что на практике делали (от этого кода). 
 
 test('Недостающие страницы', async ({page}) => {
+    await page.goto('')
+    // скрин faq
+    await page.getByTestId('header-nav-link-faq').click()
+    await expect.soft(page).toHaveScreenshot('faq.png')
 
+    // скрин акций
+    await page.getByTestId('header-nav-link-promotions').click()
+    await expect.soft(page).toHaveScreenshot('promotions.png')
 })
 
 test('Пустая корзина', async ({page}) => {
@@ -28,7 +61,7 @@ test('Корзина с товаром', async ({page}) => {
     await page.getByTestId('catalog-add-to-cart-button-prod-001').click()
     await page.getByTestId('catalog-add-to-cart-button-prod-001').click()
     await page.getByTestId('header-cart-button').click();
-    await expect.soft(page).toHaveScreenshot({
+    await expect.soft(page).toHaveScreenshot('Корзина с товаром.png', {
         fullPage: true,
         mask: [
             page.getByTestId('feedback-captcha-image'),
